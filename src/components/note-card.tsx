780164677f6ea6cbcf3c1b9e3338f9b8aaf00927
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-02-12 21:20:09 -0300

    create notes

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,13 +1,21 @@
-export function NoteCard() {
+interface NoteCardProps {
+   note: {
+    date: Date
+    content: string
+   }
+
+} 
+
+export function NoteCard({ note }: NoteCardProps) {
     return (
-        <div className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+        <button className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
             <span className="text-sm font-medium text-slate-300">
-                há 2 dias
+                {note.date.toISOString()}
             </span>
             <p className="text-sm leading-6 text-slate-400">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure porro repellendus molestias earum. Repellat incidunt magnam cupiditate dolorem perferendis at officia, omnis, necessitatibus, saepe eveniet voluptas earum quo doloremque eius?
-             </p>
+                {note.content}
+            </p>
             <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0" />  
-        </div>
+        </button>
     );
 }
